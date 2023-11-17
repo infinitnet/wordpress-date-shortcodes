2d8727f3704e1284c23ae071329355b14f12c891
infinitnet <co@infinitnet.io>
2023-11-17 13:06:01 +0100

    v1.2 release: adjust parameter rolled out to all shortcodes

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ The main purpose of this plugin is to dynamically add dates to post titles. The 
 
 You can use the `adjust` parameter for `[currentday]` to display the date for tomorrow, yesterday, the day after tomorrow, etc. This can be useful to make discounts always expire "tomorrow", for example: "Hurry up! This discount expires on `[currentmonth] [currentday adjust="+1"]`!"
 
+All other shortcodes also support the `adjust` parameter, e.g. `[currentmonth adjust="+1"]` will show the month after the current one, `[currentyear adjust="+1"]` will show the next year, etc.
+
 ## Available Shortcodes
-- **[currentday]** - current day
 - **[currentday adjust="+1"]** - tomorrow (both `+` and `-` are supported)
+- **[currentday]** - current day
 - **[currentmonth]** - current month
 - **[currentyear]** - current year
 - **[publishedday]** - day the post/page was published
@@ -17,6 +19,8 @@ You can use the `adjust` parameter for `[currentday]` to display the date for to
 - **[modifiedday]** - day the post/page was last modified
 - **[modifiedmonth]** - month the post/page was last modified
 - **[modifiedyear]** - year the post/page was last modified
+
+**Note:** The `adjust` parameter is supported by all shortcodes and can use both `+` and `-` depending on whether you want to display future or past dates.
 
 ## Compatibility
 The WordPress Date Shortcodes plugin is compatible with:
--- wordpress-date-shortcodes.php
+++ wordpress-date-shortcodes.php
@@ -6,30 +6,41 @@
  * Author URI: https://infinitnet.io/
  * Plugin URI: https://github.com/infinitnet/wordpress-date-shortcodes
  * Update URI: https://github.com/infinitnet/wordpress-date-shortcodes
- * Version: 1.1
+ * Version: 1.2
  * License: GPLv3
  * Text Domain: wordpress-date-shortcodes
  */
 
 // Main function
 if ( ! function_exists( 'infinitnet_date_shortcode' ) ) {
-    function infinitnet_date_shortcode( $type, $format, $adjustment = 0, $post_date = '' ) {
+    function infinitnet_date_shortcode( $type, $format, $atts ) {
+        $atts = shortcode_atts( array('adjust' => 0), $atts );
+        $adjustment = intval($atts['adjust']);
+
         switch ( $type ) {
             case 'current':
-                $date = date_i18n($format, strtotime("$adjustment days"));
+                if ($format == 'Y') {
+                    $adjusted_date = strtotime("$adjustment year");
+                } elseif ($format == 'F') {
+                    $adjusted_date = strtotime("$adjustment month");
+                } else {
+                    $adjusted_date = strtotime("$adjustment day");
+                }
+                $date = date_i18n($format, $adjusted_date);
                 break;
             case 'published':
-                $date = get_the_date($format, get_the_ID());
-                break;
             case 'modified':
-                $date = get_the_modified_date($format, get_the_ID());
+                $post_date = ($type == 'published') ? get_the_date('Y-m-d', get_the_ID()) : get_the_modified_date('Y-m-d', get_the_ID());
+                if ($format == 'Y') {
+                    $date = date_i18n($format, strtotime("$post_date +$adjustment year"));
+                } elseif ($format == 'F') {
+                    $date = date_i18n($format, strtotime("$post_date +$adjustment month"));
+                } else {
+                    $date = date_i18n($format, strtotime("$post_date +$adjustment day"));
+                }
                 break;
             default:
                 $date = '';
-        }
-
-        if ($type !== 'current' && $adjustment !== 0) {
-            $date = date_i18n($format, strtotime("$post_date $adjustment days"));
         }
 
         return $date;
@@ -37,30 +48,27 @@ if ( ! function_exists( 'infinitnet_date_shortcode' ) ) {
 }
 
 // Year shortcodes
-add_shortcode( 'currentyear', function() { return infinitnet_date_shortcode('current', 'Y'); });
-add_shortcode( 'publishedyear', function() { return infinitnet_date_shortcode('published', 'Y'); });
-add_shortcode( 'modifiedyear', function() { return infinitnet_date_shortcode('modified', 'Y'); });
+add_shortcode( 'currentyear', function($atts) { return infinitnet_date_shortcode('current', 'Y', $atts); });
+add_shortcode( 'publishedyear', function($atts) { return infinitnet_date_shortcode('published', 'Y', $atts); });
+add_shortcode( 'modifiedyear', function($atts) { return infinitnet_date_shortcode('modified', 'Y', $atts); });
 
 // Month shortcodes
-add_shortcode( 'currentmonth', function() { return infinitnet_date_shortcode('current', 'F'); });
-add_shortcode( 'publishedmonth', function() { return infinitnet_date_shortcode('published', 'F'); });
-add_shortcode( 'modifiedmonth', function() { return infinitnet_date_shortcode('modified', 'F'); });
+add_shortcode( 'currentmonth', function($atts) { return infinitnet_date_shortcode('current', 'F', $atts); });
+add_shortcode( 'publishedmonth', function($atts) { return infinitnet_date_shortcode('published', 'F', $atts); });
+add_shortcode( 'modifiedmonth', function($atts) { return infinitnet_date_shortcode('modified', 'F', $atts); });
 
 // Day shortcodes
 add_shortcode( 'currentday', function($atts) {
-    $atts = shortcode_atts( array('adjust' => 0), $atts, 'currentday' );
     $day_format = preg_match('/[jS]/', get_option('date_format'), $day_only_format) ? $day_only_format[0] : 'j';
-    return infinitnet_date_shortcode('current', $day_format, intval($atts['adjust']));
+    return infinitnet_date_shortcode('current', $day_format, $atts);
 });
 add_shortcode( 'publishedday', function($atts) {
-    $atts = shortcode_atts( array('adjust' => 0), $atts, 'publishedday' );
     $day_format = preg_match('/[jS]/', get_option('date_format'), $day_only_format) ? $day_only_format[0] : 'j';
-    return infinitnet_date_shortcode('published', $day_format, intval($atts['adjust']), get_the_date('Y-m-d'));
+    return infinitnet_date_shortcode('published', $day_format, $atts);
 });
 add_shortcode( 'modifiedday', function($atts) {
-    $atts = shortcode_atts( array('adjust' => 0), $atts, 'modifiedday' );
     $day_format = preg_match('/[jS]/', get_option('date_format'), $day_only_format) ? $day_only_format[0] : 'j';
-    return infinitnet_date_shortcode('modified', $day_format, intval($atts['adjust']), get_the_modified_date('Y-m-d'));
+    return infinitnet_date_shortcode('modified', $day_format, $atts);
 });
 
 // Unified function to process meta content with shortcodes
